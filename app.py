e19e167a943e3d87fde78d34379c051bd6e1df67
brunotizer <brunomateus160601@gmail.com>
2025-05-08 22:43:32 -0300

    Fix CORS origin format and restructure app run configuration

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import negocio
-
+import os
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1.5000"])
+CORS(app, origins=["http://127.0.0.1:5000"])
 
 @app.route("/perguntas/<int:enquete_id>", methods=["GET"])
 @cross_origin()
 def recupera_perguntas(enquete_id: int):
-	dados = negocio.recupera_perguntas(enquete_id)
-	return (jsonify(dados), 200)
+    dados = negocio.recupera_perguntas(enquete_id)
+    return jsonify(dados), 200
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
